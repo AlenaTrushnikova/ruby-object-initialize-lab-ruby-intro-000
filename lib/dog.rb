82643d055da57104db9c1aa835d0b1745cba5270
Alena Trushnikova <alena.yu.t@gmail.com>
2020-09-15 05:04:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -13,12 +13,12 @@ class Dog
   def initialize(breed)
     @breed = breed
   end
-  def breed=(breed)
-    @breed = breed
-  end
-  def breed
-    @breed
-  end
+  #def breed=(breed)
+   # @breed = breed
+  #end
+ # def breed
+  #  @breed
+ # end
   
   def initialize(name, breed = "Mutt")
     @name = name
